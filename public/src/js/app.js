4e071cda2a7f5484183e02cd32ec52e4a3cc89b6
Alejandro Steinmetz <asteinmetz78@gmail.com>
2017-09-10 19:39:55 -0300

    avance

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,3 +1,5 @@
+var deferredPrompt;
+
 if ('serviceWorker' in navigator) {
 	navigator.serviceWorker
 		.register('/sw.js')
@@ -5,3 +7,9 @@ if ('serviceWorker' in navigator) {
 			console.log("Service worker registered!")
 		})
 }
+window.addEventListener('beforeinstallprompt', event => {
+	console.log("beforeinstallprompt fired")
+	event.preventDefault()
+	deferredPrompt = event
+	return false
+})
